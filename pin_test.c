d93c68169a7e6cc13c0b7dee7c63e9c0a49e8082
jhold94 <33321625+jhold94@users.noreply.github.com>
2017-11-20 13:10:11 -0500

    Update pin_test.c

--- pin_test.c
+++ pin_test.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
@@ -17,9 +18,67 @@
 #include <errno.h>
 #include <math.h>
 #include <getopt.h>
+
 #include "gpiolib.h"
 #include "fpga.h"
 #include "crossbar-ts7680.h"
+#include "12c-dev.h"
+
+
+
+/********************************************************************************/
+// fpga dependencies
+/********************************************************************************/
+int fpga_init(char *path, char adr)
+{
+        static int fd = -1;
+
+        if(fd != -1)
+                return fd;
+
+        if(path == NULL) {
+                // Will always be I2C0 on the 7680
+                fd = open("/dev/i2c-0", O_RDWR);
+        } else {
+
+                fd = open(path, O_RDWR);
+        }
+
+        if(!adr) adr = 0x28;
+
+        if(fd != -1) {
+                if (ioctl(fd, I2C_SLAVE_FORCE, 0x28) < 0) {
+                        perror("FPGA did not ACK 0x28\n");
+                        return -1;
+                }
+        }
+
+        return fd;
+}
+
+void fpoke8(int twifd, uint16_t addr, uint8_t value)
+{
+        uint8_t data[3];
+        data[0] = ((addr >> 8) & 0xff);
+        data[1] = (addr & 0xff);
+        data[2] = value;
+        if (write(twifd, data, 3) != 3) {
+                perror("I2C Write Failed");
+        }
+}
+
+uint8_t fpeek8(int twifd, uint16_t addr)
+{
+        uint8_t data[2];
+        data[0] = ((addr >> 8) & 0xff);
+        data[1] = (addr & 0xff);
+        if (write(twifd, data, 2) != 2) {
+                perror("I2C Address set Failed");
+        }
+        read(twifd, data, 1);
+
+        return data[0];
+}
 
 
 /********************************************************************************/
