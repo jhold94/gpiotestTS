e77c5bdb1c576252c9da90ceda63956d2ed6caf5
jhold94 <33321625+jhold94@users.noreply.github.com>
2017-11-20 13:28:35 -0500

    Update pin_test.c

--- pin_test.c
+++ pin_test.c
@@ -352,7 +352,7 @@ int main(int argc, char **argv)
                 { 0, 0, 0, 0 }
         };
                 
-        while((c = getopt_long(argc, argv, "+h:tmi:o:e:j:l:a:b:c:d:pqrswxyzg:", 
+        while((c = getopt_long(argc, argv, "+h:i:t:m:o:e:j:l:a:b:c:d:g:p:q:r:s:w:x:y:z:", 
           long_options, NULL)) != -1) {
                 int gpio;
                 
